c0ee1f7644f13333b0f7d9dae7d424c91f7c3a21
benjaminpeto <benjaminpeto23@gmail.com>
2026-02-10 21:41:52 +0000

    deploy: set app base for GitHub Pages deployment

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -20,7 +20,7 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <CartProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/indi-cell-spa">
           <AppRoutes />
         </BrowserRouter>
       </CartProvider>
