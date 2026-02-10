ff9fb4728eaa395be9de4ab936c5bf67c7ffffa9
benjaminpeto <benjaminpeto23@gmail.com>
2026-02-10 22:10:14 +0000

    chore: remove github-pages artifacts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/indi-cell-spa/',
   plugins: [react(), tailwindcss()],
   test: {
     globals: true,
